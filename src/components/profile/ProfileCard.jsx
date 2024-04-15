bbd5d9900916538eba620606233fceb6617c3035
frirsta <frirsta.karem@hantverkshjalpenonline.se>
2024-04-15 06:01:39 +0200

    Enhance ProfileCard with user-specific actions and authentication context
    - Integrated  to enable conditional rendering based on the authentication state within the  component.
    - Added dynamic display elements that change based on whether the viewed profile belongs to the current user, including editable options for the user's own profile and follow/message options for other users.
    - Implemented tooltips for upcoming features, enhancing user interaction and providing feedback for future functionality.

--- src/components/profile/ProfileCard.jsx
+++ src/components/profile/ProfileCard.jsx
@@ -1,15 +1,18 @@
-import React from "react";
+import React, { useContext } from "react";
 import useMediaQuery from "@mui/material/useMediaQuery";
 import Button from "@mui/joy/Button";
 import Box from "@mui/joy/Box";
 import Grid from "@mui/joy/Grid";
 import Avatar from "@mui/joy/Avatar";
 import Typography from "@mui/joy/Typography";
+import Tooltip from "@mui/joy/Tooltip";
 import PostFollowerCount from "./PostFollowerCount";
+import { AuthContext } from "../../context/AuthContext";
 
-const ProfileCard = () => {
+const ProfileCard = ({ username, uid, followers, posts, following }) => {
   const isMobileScreen = useMediaQuery("(max-width:600px)");
   const isTabletScreen = useMediaQuery("(min-width:900px)");
+  const { currentUser } = useContext(AuthContext);
   return (
     <Box
       sx={{
@@ -47,39 +50,68 @@ const ProfileCard = () => {
             <Avatar sx={{ height: "150px", width: "150px" }} />
           )}
         </Grid>
-        <Grid
-          xs={8}
-          sm={7}
-          lg={5}
-          spacing={1}
-          container
-          sx={{
-            flexGrow: 1,
-            display: "flex",
-          }}
-          justifyContent={{ xs: "flex-start", md: "flex-end" }}
-          alignItems={{ xs: "center", md: "flex-start" }}
-        >
-          <Grid md={2} xs={12}>
-            <Typography fontWeight="bold">Username</Typography>
+        {currentUser?.uid === uid ? (
+          <Grid
+            xs={8}
+            sm={7}
+            lg={5}
+            spacing={1}
+            container
+            sx={{
+              flexGrow: 1,
+              display: "flex",
+            }}
+            justifyContent={{ xs: "flex-start", md: "flex-end" }}
+            alignItems={{ xs: "center", md: "flex-start" }}
+          >
+            <Grid md={2} xs={12}>
+              <Typography fontWeight="bold">{username}</Typography>
+            </Grid>
+            <Grid md={3} xs={6}>
+              <Button size="sm" sx={{ width: "100%" }}>
+                Edit Profile
+              </Button>
+            </Grid>
+            <Grid md={3} xs={6}>
+              <Button size="sm" sx={{ width: "100%" }}>
+                2
+              </Button>
+            </Grid>
+            <Grid md={3} xs={12}>
+              <Button size="sm" sx={{ width: "100%" }}>
+                3
+              </Button>
+            </Grid>
+            {isTabletScreen && <PostFollowerCount />}
           </Grid>
-          <Grid md={3} xs={6}>
-            <Button size="sm" sx={{ width: "100%" }}>
-              Edit Profile
-            </Button>
+        ) : (
+          <Grid
+            xs={8}
+            sm={7}
+            lg={5}
+            spacing={1}
+            container
+            sx={{
+              flexGrow: 1,
+              display: "flex",
+            }}
+            justifyContent={{ xs: "flex-start", md: "flex-end" }}
+            alignItems={{ xs: "center", md: "flex-start" }}
+          >
+            <Grid md={3} xs={6}>
+              <Button size="sm" sx={{ width: "100%" }}>
+                Follow
+              </Button>
+            </Grid>
+            <Grid md={3} xs={6}>
+              <Tooltip title="Coming soon!" variant="plain">
+                <Button size="sm" sx={{ width: "100%" }}>
+                  Message
+                </Button>
+              </Tooltip>
+            </Grid>
           </Grid>
-          <Grid md={3} xs={6}>
-            <Button size="sm" sx={{ width: "100%" }}>
-              2
-            </Button>
-          </Grid>
-          <Grid md={3} xs={12}>
-            <Button size="sm" sx={{ width: "100%" }}>
-              3
-            </Button>
-          </Grid>
-          {isTabletScreen && <PostFollowerCount />}
-        </Grid>
+        )}
       </Grid>
     </Box>
   );
